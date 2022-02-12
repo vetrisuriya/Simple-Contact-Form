30eadd6687567fedd98a44b5ebd7e440271bbf7b
Vetri Suriya <34440110+vetrisuriya@users.noreply.github.com>
2022-02-12 10:27:34 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $con = new PDO("mysql:host=localhost;dbname=contact_form", "root", "");
+    $con = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_be25953a2edc963", "bbe4c1d9ae5f22", "2c957ab4");
 }
 catch(Exception $e) {
     echo $e->getMessage();
